b0f48c2a8a8bc4d3687f81e8062f069280a14c6d
Mckena Ullman <ullman.kena@gmail.com>
2022-01-13 21:42:35 -0700

    struggling with unit test

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,18 @@
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test('array should contain crowbar & the hammer ', () => {
+        expect(shuffleArray).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({name: 'The Hammer'}),
+                expect.objectContaining({name: 'crowbar'})
+            ])
+        )
+    })
+})
+
+describe('shuffleArray should', () => {
+    test('should be type array', () => {
+        expect(typeof shuffleArray).toBe('array')
+    })
 })
